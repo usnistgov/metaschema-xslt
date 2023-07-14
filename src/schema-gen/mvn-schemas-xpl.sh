1c409aca3f6d026c39b77b79400572dccbc71457
Nikita Wootten <nikita.wootten@nist.gov>
2023-07-14 09:51:07 -0400

    Corrected command line argument structure

--- src/schema-gen/mvn-schemas-xpl.sh
+++ src/schema-gen/mvn-schemas-xpl.sh
@@ -11,7 +11,7 @@ Usage: ${BASE_COMMAND:-$(basename "${BASH_SOURCE[0]}")} METASCHEMA_XML OUTPUT_DI
 Produces XML Schema (XSD) and JSON Schema from Metaschema XML source, using XML Calabash invoked from Maven.
 
 SCHEMA_NAME specifies the prefix output files will be written to, for example:
-    outdir/a-metaschema -> outdir/a-metaschema_schema.xsd, outdir/a-metaschema_schema.json
+    a-metaschema -> a-metaschema_schema.xsd, a-metaschema_schema.json
 
 Additional arguments for XML Calabash should be specified in the 'key=value' format.
 EOF
@@ -19,15 +19,17 @@ EOF
 
 [[ -z "${1-}" ]] && { echo "Error: METASCHEMA_XML not specified"; usage; exit 1; }
 METASCHEMA_XML=$1
-[[ -z "${2-}" ]] && { echo "Error: SCHEMA_NAME not specified"; usage; exit 1; }
-SCHEMA_NAME=$2
+[[ -z "${2-}" ]] && { echo "Error: OUTPUT_DIR not specified"; usage; exit 1; }
+OUTPUT_DIR=$2
+[[ -z "${3-}" ]] && { echo "Error: SCHEMA_NAME not specified"; usage; exit 1; }
+SCHEMA_NAME=$3
 
-ADDITIONAL_ARGS=$(shift 2; echo "${*// /\\ }")
+ADDITIONAL_ARGS=$(shift 3; echo "${*// /\\ }")
 
 PIPELINE="${SCRIPT_DIR}/METASCHEMA-ALL-SCHEMAS.xpl"
 
-XSD_FILE="${SCHEMA_NAME}_schema.xsd"
-JSONSCHEMA_FILE="${SCHEMA_NAME}_schema.json"
+XSD_FILE="${OUTPUT_DIR}/${SCHEMA_NAME}_schema.xsd"
+JSONSCHEMA_FILE="${OUTPUT_DIR}/${SCHEMA_NAME}_schema.json"
 
 CALABASH_ARGS="-i METASCHEMA=\"$METASCHEMA_XML\" \
                -o INT_0_echo-input=/dev/null \
@@ -37,21 +39,22 @@ CALABASH_ARGS="-i METASCHEMA=\"$METASCHEMA_XML\" \
                -o OUT_xml-schema=\"$XSD_FILE\" \
                $ADDITIONAL_ARGS \"$PIPELINE\""
 
-# Ensure the base directory exists
-mkdir -p "$(dirname "$SCHEMA_NAME")"
+# Ensure the output directory exists
+mkdir -p "$OUTPUT_DIR"
 
-if [ -e "$XSD_FILE" ]; then 
-    echo "Deleting prior $XSD_FILE..." >&2
+if [ -e "$XSD_FILE" ]; then
+    echo "Overwriting prior $XSD_FILE..." >&2
     rm -f "$XSD_FILE"
 fi
 
-if [ -e "$JSONSCHEMA_FILE" ]; then 
-    echo "Deleting prior $JSONSCHEMA_FILE..." >&2
+if [ -e "$JSONSCHEMA_FILE" ]; then
+    echo "Overwriting prior $JSONSCHEMA_FILE..." >&2
     rm -f "$JSONSCHEMA_FILE"
 fi
 
 invoke_calabash "${CALABASH_ARGS}"
 
-if [ -e "$XSD_FILE" ] && [ -e "$JSONSCHEMA_FILE" ]; then 
-    echo "Results can be found in $XSD_FILE and $JSONSCHEMA_FILE" >&2
+if [ -e "$XSD_FILE" ] && [ -e "$JSONSCHEMA_FILE" ]; then
+    echo "Wrote XSD to $XSD_FILE" >&2
+    echo "Wrote JSON schema to $JSONSCHEMA_FILE" >&2
 fi
